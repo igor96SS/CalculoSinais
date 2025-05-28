f1606a18d23c4e2fb54b6e49330c376b3f464161
Igor Simões <igorsimoes_96@hotmail.com>
2025-05-28 17:32:55 +0100

    calibration value activity created

---
+++ app/src/main/java/team/iscode/igor/calculosinais/CalibrationValuesActivity.kt
@@ -0,0 +1,15 @@
+package team.iscode.igor.calculosinais
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CalibrationValuesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calibration_values)
+
+    }
+}
