824a0daefe068d3c882a2e70df3f80a2a17fba8a
Igor Simões <igorsimoes_96@hotmail.com>
2025-05-29 18:49:58 +0100

    models for recycler views

---
+++ app/src/main/java/team/iscode/igor/calculosinais/models/CalibrationValues.kt
@@ -0,0 +1,3 @@
+package team.iscode.igor.calculosinais.models
+
+data class CalibrationValues(var percentage: Int, var inputValues: Float, var maxPercentError: Int, var acceptanceLimit: Float)
---
+++ app/src/main/java/team/iscode/igor/calculosinais/models/VerificationValues.kt
@@ -0,0 +1,3 @@
+package team.iscode.igor.calculosinais.models
+
+data class VerificationValues(var outputValues: Float, var readValues: Float, var error: Float)
