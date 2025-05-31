d50047aebbbc09a30756e05d9e3ca0b42c9fcb56
Igor Simões <igorsimoes_96@hotmail.com>
2025-06-01 00:22:55 +0100

    minor change

--- app/src/main/java/team/iscode/igor/calculosinais/models/VerificationValues.kt
+++ app/src/main/java/team/iscode/igor/calculosinais/models/VerificationValues.kt
@@ -1,3 +1,3 @@
 package team.iscode.igor.calculosinais.models
 
-data class VerificationValues(var outputValues: Float, var readValues: Float, var error: Float)
+data class VerificationValues(var outputValues: Float, var readValues: Float?, var error: Float)
