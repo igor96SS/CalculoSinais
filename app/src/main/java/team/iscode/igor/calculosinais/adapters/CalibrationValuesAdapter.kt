03cdb8aa8601b0eb5c77c5bdf2ec3d1f643624ca
Igor Simões <igorsimoes_96@hotmail.com>
2025-05-31 19:42:03 +0100

    remove values

--- app/src/main/java/team/iscode/igor/calculosinais/adapters/CalibrationValuesAdapter.kt
+++ app/src/main/java/team/iscode/igor/calculosinais/adapters/CalibrationValuesAdapter.kt
@@ -1,6 +1,5 @@
 package team.iscode.igor.calculosinais.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,15 +7,12 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import team.iscode.igor.calculosinais.R
-import team.iscode.igor.calculosinais.adapters.ItemAdapter.InputViewHolder
 import team.iscode.igor.calculosinais.models.CalibrationValues
 import java.util.ArrayList
 
 class CalibrationValuesAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var calibrationList: List<CalibrationValues> = ArrayList()
-    private var maxPercentError: Int = 0
-    private var acceptanceLimit: Float = 0.0f
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return InputViewHolder(
@@ -41,15 +37,10 @@ class CalibrationValuesAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
     class InputViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val percentageET: EditText = itemView.findViewById(R.id.percentageValueET)
         private val signalTV: TextView = itemView.findViewById(R.id.signalValueTv)
-        private val maxErrorEt: EditText = itemView.findViewById(R.id.maxErrorPercentEt)
-        private val acceptanceLimitTv: TextView = itemView.findViewById(R.id.acceptanceLimitTv)
 
         fun bind(item: CalibrationValues) {
             percentageET.setText(item.percentage.toString())
             signalTV.text = item.inputValues.toString()
-
-            maxErrorEt.setText("")
-            acceptanceLimitTv.text = 0.toString()/* valor calculado */
         }
     }
 
